33353fffeea66d508b35e6b393024b5acd742a65
Faruk <farukomererdem2003@gmail.com>
2025-08-04 14:21:23 +0300

    agent class

---
+++ server/agent.py
@@ -0,0 +1,40 @@
+import json
+import os
+from logger import get_logger
+
+logger=get_logger()
+
+class Agent:
+    def __init__(self, addr: str,
+                 roles: list,
+                 json_file: str) -> None:
+        self.addr = addr
+        self.roles = roles
+        self.json_file = json_file
+        self._save_to_json()
+
+    def to_dict(self):
+        """Convert object to dictionary"""
+        return {
+            "addr": self.addr,
+            "roles": self.roles
+        }
+
+    def _save_to_json(self):
+        """Write object to JSON file"""
+        data = []
+        if os.path.exists(self.json_file):
+            with open(self.json_file, 'r') as f:
+                try:
+                    data = json.load(f)
+                except:
+                    data = []
+        data.append(self.to_dict())
+
+        if self.to_dict() in data:
+           logger.debug(f"{self.addr} already exists in agents.") 
+        else:
+            logger.info(f"{self.addr} doesn't exist in agents, adding it to JSON.")
+            with open(self.json_file, 'w') as f:
+                json.dump(data, f, ensure_ascii=False)
+
